f108defed947d45a3d379aade8c0c3cfe88fe472
dresser <matthew.dresser@valtech.com>
2016-07-10 20:11:16 +0100

    Tidied-up slide changing code.

--- Src/Web/Assets/Scripts/carousel.js
+++ Src/Web/Assets/Scripts/carousel.js
@@ -1,49 +1,35 @@
-﻿function prevSlide(data) {
+﻿function setCurrentSlide(index) {
     var slides = $('.carousel .carousel-slide');
     slides.removeClass('visible');
+    $('.carousel').data('current-slide', index);
+    $(slides[index]).addClass('visible');
+}
+
+function prevSlide() {
     var curSlide = $('.carousel').data('current-slide');
     if (curSlide > 0) {
         curSlide--;
     } else {
         curSlide = $('.carousel').data('slide-count') - 1;
     }
-    $('.carousel').data('current-slide', curSlide);
-    $(slides[curSlide]).addClass('visible');
+    setCurrentSlide(curSlide);
 }
 
-function nextSlide(data) {
-    var slides = $('.carousel .carousel-slide');
-    var slideCount = $('.carousel').data('slide-count');
-    slides.removeClass('visible');
+function nextSlide() {
     var curSlide = $('.carousel').data('current-slide');
-    if (curSlide > (slideCount - 2)) {
+    if (curSlide > ($('.carousel').data('slide-count') - 2)) {
         curSlide = 0;
     } else {
         curSlide++;
     }
-    $('.carousel').data('current-slide', curSlide);
-    $(slides[curSlide]).addClass('visible');
-}
-
-function isPageEditorMode() {
-    if (typeof Sitecore == "undefined") {
-        return false;
-    }
-    if (typeof Sitecore.PageModes == "undefined" || Sitecore.PageModes == null) {
-        return false;
-    }
-    return Sitecore.PageModes.PageEditor != null;
+    setCurrentSlide(curSlide);
 }
 
 $(function () {
-    $('.carousel > .carousel-slide:first-child').addClass('visible');
-    $('.carousel').data('slide-count', $('.carousel > .carousel-slide').length);
-    $('.carousel').data('current-slide', 0);
+    var curSlide = 0;
+    setCurrentSlide(curSlide);
+    $('.carousel').data('current-slide', curSlide);
+    $('.carousel').data('slide-count', $('.carousel .carousel-slide').length);
     $('.carousel .prev .arrow').on('click', prevSlide);
     $('.carousel .next .arrow').on('click', nextSlide);
-    if (isPageEditorMode() == false) {
-        //window.setInterval(changeSlide, 3000);
-    } else {
-        $('.carousel').addClass("edit");
-    }
 });
